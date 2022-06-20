951a62a462619c9b1b0f6d1ca3c1ebf6227732a3
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-06-20 14:42:44 -0500

    updating

--- 1JS/Week4-ECMASAndInterJS/ES6ArrowFunctions.js
+++ 1JS/Week4-ECMASAndInterJS/ES6ArrowFunctions.js
@@ -49,3 +49,109 @@ so the value of the function won't be changed later on in the code.
  */
 
 
+//!OPEN CLASS QUESTIONS:
+
+/*Convert the following function into an arrow function. 
+Refer to the new function as monthlyPayment. 
+
+function myFunction(yearlyPayment) {
+    let monthly = yearlyPayment / 12;
+    return  monthly.toFixed(2);
+}
+*/
+//!MY ANSWER:
+var a = 1100;
+
+//write your code here
+// const monthlyPayment = yearlyPayment => {
+//     const months = 12;
+//     const monthly = (yearlyPayment / 12);
+//     return monthly.toFixed(2);
+// };
+//! INSTRUCTORS:
+//* line 74 consolidated line 67 & 68
+let monthlyPayment = (yearlyPayment) =>{
+    let monthly = yearlyPayment / 12;
+    return monthly.toFixed(2);
+};
+//do not change
+//for testing purposes
+console.log(monthlyPayment(a));
+
+//!NEXT QUESTION
+/* Convert the following function into an arrow function. 
+Refer to the new function as revert.
+
+function myFunction(value){ return !value; }
+ */
+//! MY ANSWER:
+var a = 0;
+//write your code here
+const revert = a => !a; 
+//! INSTRUCTORS
+// let revert = (value) => !value;
+
+//do not change
+console.log(revert(a));
+
+//! NEXT QUESTION
+/* Convert the following function into an arrow function. 
+Refer to the new function as xor.
+
+function myFunction(num1, num2) {
+    return num1 ^ num2;
+}
+ */
+//! MY ANSWER:
+var a = 4;
+var b = 6;
+//write your code here
+const xor = (a, b) => a ^ b;
+//do not change
+//for testing purposes
+console.log(xor(a,b));
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_XOR
+
+//! NEXT QUESTION
+/* Convert the following function into an arrow function. 
+Refer to the new function as fancyAlgorithm.
+
+function myFunction(num1, num2, num3) {
+    let value = 0;
+    for (let i = 0; i < num3; i++) {
+        value = (value + num2) * num1;
+    }
+    return value / (num1 * (num3 * 10));
+}
+ */
+//! MY ANSWER:
+var a = 1;
+var b = 2;
+var c = 3;
+//write your code here
+const fancyAlgorithm = (num1, num2, num3) => {
+    let value = 0;
+    for (let i = 0; i < num3; i++) {
+        value = (value + num2) * num1;
+    }
+    return value / (num1 * (num3 * 10));
+};
+
+console.log(fancyAlgorithm(a, b, c));
+
+//! NEXT QUESTION
+/* Convert the following function into an arrow function. 
+Refer to the new function as pythagoras.
+
+function myFunction(num1, num2) { 
+    return Math.sqrt((num1*num2)+(num2*num2)); 
+}
+ */
+//! MY ANSWER:
+var a = 1;
+var b = 2;
+//write your code here
+let pythagoras = (num1, num2) => Math.sqrt((num1*num2)+(num2*num2));
+//do not change
+//for testing purposes
+console.log(pythagoras(a,b));
