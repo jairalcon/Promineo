ea6e5119475adc10abcb8560d407126909996a05
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-06-21 13:32:31 -0500

    adding challenge from fCC

--- 1JS/Week4-ECMASAndInterJS/ES6ArrowFunctions.js
+++ 1JS/Week4-ECMASAndInterJS/ES6ArrowFunctions.js
@@ -155,3 +155,26 @@ let pythagoras = (num1, num2) => Math.sqrt((num1*num2)+(num2*num2));
 //do not change
 //for testing purposes
 console.log(pythagoras(a,b));
+
+
+
+
+//! FROM freeCodeCamp:
+/* Problem 1: Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number.*/
+
+const multiplesOf3and5 = (number) => {
+    let sum = 0,
+        i = 3;
+    while (i < number) {
+        if (i % 3 == 0 || i % 5 == 0) sum += i;
+        i++;
+    }
+    return sum;
+}
+  
+console.log('This is the sum of all the multiples of 3 or 5:', multiplesOf3and5(49));
