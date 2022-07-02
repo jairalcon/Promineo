347a4236d0cd3d2512781a4d12a148dd3ea4dbc2
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-07-01 19:09:05 -0500

    adding updating notes, almost ready to submit

--- 1JS/Week5-ObjectOrtientedProgramming/Week5CodingAssignment/Week5CA.js
+++ 1JS/Week5-ObjectOrtientedProgramming/Week5CodingAssignment/Week5CA.js
@@ -4,7 +4,7 @@
 /*
 1.	Create a menu app as seen in this week's video. What you create is up to you as long as it meets the following requirements.
     a.	Use at least one array.
-todo    b.	Use at least two classes.
+    b.	Use at least two classes.
     c.	Your menu should have the options to create, view, and delete elements.
 */
 
@@ -56,19 +56,18 @@ class Menu {
         `);
     }
 
+    //
     createPhone() {
         // requesting user to enter both data types in separate prompts
         let name = prompt('Enter name of phone');
         let storage = prompt('Enter name storage size of phone (GB)');
         // after data is entered, then it's pushed into phones array
         this.phones.push(new Phone(name, storage));
-
+        //logging what createPhone pushed into "phones" array"
         console.log(`This phone was just added to the phones array:`, new Phone(name, storage));
     }
 
-
-    
-    //created a function that returns a string
+    //created a method that returns a list of available phones
     availablePhones() {
         let phoneNamesList = '';
         // for loop to iterate through current array of available phones
@@ -85,14 +84,22 @@ class Menu {
         let index = prompt (`Enter the index of the phone you wish to delete: ${this.availablePhones()}`);
         // index is grabbing/finding data based off input
         if (index > -1 && index < this.phones.length) {
-            // once data is found, then log/alert 
+            // once data is found, then log in console/alert 
             console.log(`${this.phones[index].name} - ${this.phones[index].storage}GB phone was deleted.`);
             alert(`${this.phones[index].name} - ${this.phones[index].storage}GB phone was deleted.`);
             // after log/alert, then delete data/perform code
-            this.phones.splice(index, 1);     
+            this.phones.splice(index, 1);
+        }
+
+        else {
+            console.log(`Entered invalid index: ${this.phones.length} exist(s) \n
+            Try again but enter ${this.phones.length - 1} or 0`);
+            alert (`You can only delete a phone that exists: ${this.phones.length} exist(s) \n
+            Try again but enter ${this.phones.length - 1} or 0`);
         }
     }
 
+    // method to display current phones in "phones" array
     displayPhones() {
         let phoneString = '';
         for (let i = 0; i < this.phones.length; i++) {
@@ -108,6 +115,7 @@ class Menu {
 let menu = new Menu();
 // menu.start();
 
-let test = new Phone('iPhone', 250);
-test.createPhone();
-console.log(test);
+
+// testing code
+// let test = new Phone('iPhone', 250);
+// console.log(test);
