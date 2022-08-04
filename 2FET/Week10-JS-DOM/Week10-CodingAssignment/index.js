d697ea750ce7f5eeb60f5b5234dd5c16df085a8e
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-08-04 14:54:57 -0500

    updating code

--- 2FET/Week10-JS-DOM/Week10-CodingAssignment/index.js
+++ 2FET/Week10-JS-DOM/Week10-CodingAssignment/index.js
@@ -1,67 +1,54 @@
-// Week 10 Codign Assignment
-// Jair Alcon
-let id = 0;
+// Week	10 Codign	Assignment
+// Jair	Alcon
+let	id = 0;
 
 window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-        return console.log('pressing a key'); // Should do nothing if the default action has been cancelled
-    }
-
-    let handled = false;
-    if (event.key !== undefined) {
-        // Handle the event with KeyboardEvent.key
-        handled = true;
-    } else if (event.keyCode !== undefined) {
-        // Handle the event with KeyboardEvent.keyCode
-        handled = true;
-    }
-
-    if (handled) {
-        console.log('handled', handled)
-        // Suppress "double action" if event handled
-        event.preventDefault();
-    }
+	const p = document.createElement("p");
+	p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
+	document.getElementById("output").appendChild(p);
+	window.scrollTo(0, document.body.scrollHeight);
 }, true);
 
-document.getElementById('add').addEventListener('click', () => {
-    let createdVehicle = new Text();
-    
-    // this essentially stamps out a new container from the input text
-    let table = document.getElementById('list');
-    console.log('This is new container from input', table);
 
-    let row = table.insertRow(1);
-    console.log('This is adding another row of data from text input', row);
+document.getElementById('add').addEventListener('click', ()	=> {
+	let	createdVehicle = new Text();
+	
+	// this	essentially	stamps out a new container from	the	input	text
+	let	table	=	document.getElementById('list');
+	console.log('This	is new container from	input',	table);
 
-    row.setAttribute('id', `item-${id}`);
-        // console.log('line 11', row.setAttribute('id', `item-${id}`))
-    row.insertCell(0).innerHTML = document.getElementById('new-vehicle-make').value;
-    // console.log('line 12', row.insertCell(0).innerHTML = document.getElementById('new-vehicle-make').value)
-    row.insertCell(1).innerHTML = document.getElementById('new-vehicle-model').value;
-    row.insertCell(2).innerHTML = document.getElementById('new-vehicle-year').value;
-    let actions = row.insertCell(3);
-    console.log(actions);
-    actions.appendChild(createDeleteButton(id++));
-    document.getElementById('new-vehicle-make').value = '';
-    document.getElementById('new-vehicle-model').value = '';
-    document.getElementById('new-vehicle-year').value = '';
+	let	row	=	table.insertRow(1);
+	console.log('This	is adding	another	row	of data	from text	input',	row);
+
+	row.setAttribute('id', `item-${id}`);
+			// console.log('line 11',	row.setAttribute('id', `item-${id}`))
+	row.insertCell(0).innerHTML	=	document.getElementById('new-vehicle-make').value;
+	// console.log('line 12',	row.insertCell(0).innerHTML	=	document.getElementById('new-vehicle-make').value)
+	row.insertCell(1).innerHTML	=	document.getElementById('new-vehicle-model').value;
+	row.insertCell(2).innerHTML	=	document.getElementById('new-vehicle-year').value;
+	let	actions	=	row.insertCell(3);
+	console.log(actions);
+	actions.appendChild(createDeleteButton(id++));
+	document.getElementById('new-vehicle-make').value	=	'';
+	document.getElementById('new-vehicle-model').value = '';
+	document.getElementById('new-vehicle-year').value	=	'';
 });
 
-function createDeleteButton(id) {
-    let btn = document.createElement('button');
-    btn.className = 'btn btn-danger';
-    btn.id = id;
-    btn.innerHTML = 'Remove';
-    btn.onclick = () => {
-        console.log(`Deleting row with id: item-${id}`);
-        let elementToDelete = document.getElementById(`item-${id}`);
-        elementToDelete.parentNode.removeChild(elementToDelete);
-    };
-    return btn;
+function createDeleteButton(id)	{
+	let	btn	=	document.createElement('button');
+	btn.className	=	'btn btn-danger';
+	btn.id = id;
+	btn.innerHTML	=	'Remove';
+	btn.onclick	=	() =>	{
+			console.log(`Deleting	row	with id: item-${id}`);
+			let	elementToDelete	=	document.getElementById(`item-${id}`);
+			elementToDelete.parentNode.removeChild(elementToDelete);
+	};
+	return btn;
 }
 
-// function onSomethingTyped() {
-//     if (event.keyCode === 13) { // enter key
-        
-//     }
+// function	onSomethingTyped() {
+//		 if	(event.keyCode === 13) { //	enter	key
+			
+//		 }
 // }
