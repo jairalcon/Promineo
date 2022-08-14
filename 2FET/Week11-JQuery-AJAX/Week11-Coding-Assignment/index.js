f9508ef4f7325df70f9c4c7910b555068f133089
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-08-13 21:25:11 -0500

    updating code

--- 2FET/Week11-JQuery-AJAX/Week11-Coding-Assignment/index.js
+++ 2FET/Week11-JQuery-AJAX/Week11-Coding-Assignment/index.js
@@ -1,149 +1,143 @@
-/* Jair Alcon - Week 11 Coding Assignment */
-console.log(`-----Jair Alcon: Week 11 Coding Assignment-----`);
-
-const X_PLAYER = 'x';
-console.log('This is player:', X_PLAYER);
-
-const O_PLAYER = 'circle';
-console.log('This is player:', O_PLAYER);
-
-const WINNING_COMBOS = [
+// using these throughout app
+const X_CLASS = 'x';
+// use these throughout app
+const CIRCLE_CLASS = 'circle';
+// create array for all winning combinations
+const WINNING_COMBINATIONS = [
+    // horizontal win combinations
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
+    // vertical win combinations
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
+    // diagonal win combinations
     [0, 4, 8],
     [2, 4, 6]
-];
-console.log('These are the winning combinations:', WINNING_COMBOS);
-
-// Getting each box of the grid
-const boxElements = document.querySelectorAll('[data-box]');
-console.log('These are the box elements:', boxElements);
-
-// Gameboard as the grid of game
-const gameboard = document.getElementById('gameboard');
-console.log('This is the gameboard element:', gameboard)
-
-// selector for display message in case win/lose/draw
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
-console.log('', winningMessageTextElement);
-
-// 
+]
+// selecting all "data-cell" into variable which creates and array
+const cellElements = document.querySelectorAll('[data-cell]');
+// selecting board class and assigning to variable
+const board = document.getElementById('board');
+// selecting winning message div and assigning variable
 const winningMessageElement = document.getElementById('winningMessage');
-console.log('', winningMessageElement);
-
-// 
-const playAgain = document.getElementById('restartBtn');
-console.log(''), playAgain;
-
-// 
-const startOver = document.getElementById('startOver');
-console.log('', startOver);
-
-// 
-const turn = document.querySelector('.turn');
-console.log('', turn);
-
-// 
+// taking top red restart button and assigning variable
 const restart = document.querySelector('#restart');
-console.log('', restart);
-
+// selecting button and assigning to variable
+const restartButton = document.getElementById('restartButton');
+// selecting winning text message and assigning variable
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+// taking bottom span and assigning variable
+const turn = document.querySelector('.turn');
+// checking who's turn it is with this variable, if true it's circle's turn and visa versa
 let circleTurn;
 
-//calling function to start the game
+// calls function to start game
 startGame();
 
-// clicking on Play Again button will restart game
-restart.addEventListener('click', startGame)
-playAgain.addEventListener('click', startGame)
+// adding event listener with click and calling startGame() function
+restart.addEventListener('click', startGame) // top button
+restartButton.addEventListener('click', startGame); // button when game ends
 
+// function starts the game by clearing out the board
 function startGame() {
-    //Set to be X's Turn first
+    // setting circle turn to false so that X starts the game
     circleTurn = false;
-    // For each cell remove X & Circle
-    boxElements.forEach(box => {
-        box.classList.remove(X_PLAYER);
-        box.classList.remove(O_PLAYER);
-        box.removeEventListener('click', handleClick);
-        // Once cell is clicked in the grid this disallows change within that same box
-        box.addEventListener('click', handleClick, { once: true });
-        //! console.log(handleClick);
+    // looping over each cell
+    cellElements.forEach(cell => {
+        // resetting game board
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.removeEventListener('click', handleClick);
+        // every time we click on the cell, only active event listener once
+        cell.addEventListener('click', handleClick, { once: true })
     });
+    // enabling hover CSS when game starts
     setBoardHoverClass();
+    // removing message when resetting the game
     winningMessageElement.classList.remove('show');
 }
 
-// On click it will target box 
 function handleClick(e) {
-    const box = e.target;
-    console.log('', box);
-    // Whose turn it is assign it to be the current class
-    const currentClass = circleTurn ? O_PLAYER : X_PLAYER;
-    console.log('', currentClass);
-    // Place mark
-    placeMark(box, currentClass);
-    // Check for win
+    // Finds
+    const cell = e.target;
+    // if it's circle's turn return circle class, otherwise return x class
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    // places mark on cell and with current class
+    placeMark(cell, currentClass);
+    // Check for Win
     if (checkWin(currentClass)) {
-        endGame(false)
-    }
-    // Check for draw
-    else if (isDraw()) {
-        endGame(true)
+        // this determines that game has not ended
+        endGame(false);
+        // Check for Draw
+    } else if (isDraw()) {
+        endGame(true);
     } else {
-        // Switch turns
-        switchTurns()
-        setBoardHoverClass()
+        // Switch turns function only if there's not a winner yet
+        swapTurns();
+        // want to add this after swapTurns so we know whose turn it currently is
+        setBoardHoverClass();
     }
-
 }
-// Ends the game if the game is draw
+
+// creating function to determine if game is a draw
 function endGame(draw) {
     if (draw) {
         winningMessageTextElement.innerText = 'Draw!';
+    } else {
+        // checks to see whose current turn it is when game is won
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Win!`;
     }
-    // if game does not result in a draw, then did X or O win the game?
-    else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
-    }
-    // Displaying winning message
+    // code that actually shows the text
     winningMessageElement.classList.add('show');
 }
 
+// creating function to determine if the game is a draw 
 function isDraw() {
-    return [...boxElements].every(box => {
-        return box.classList.contains(X_PLAYER) ||
-            box.classList.contains(O_PLAYER)
+    // checking to see if every cell is filled in
+    // destructing cell elements into an array
+    return [...cellElements].every(cell => {
+        // check every cell to see if they all contain a class
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
     });
 }
-// Creating a function for placing mark on the grid
-function placeMark(box, currentClass) {
-    box.classList.add(currentClass);
-}
-// Checking whose turn it is and switching turns
-function switchTurns() {
-    circleTurn = !circleTurn;
+
+// functions that takes in cell and currentClass then adds current class mark to board
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+    console.log(`Placing ${currentClass} into this cell:`, cell);
 }
 
+// creating function takes circleTurn and then assigns the opposite
+function swapTurns() {
+    circleTurn = !circleTurn;
+    console.log(`CIRCLE'S turn = ${circleTurn}`)
+}
+
+// calling hover states
 function setBoardHoverClass() {
-    gameboard.classList.remove(X_PLAYER);
-    gameboard.classList.remove(O_PLAYER);
+    // removes all classes on the board
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    // if it's circle turn then add circle class on board
     if (circleTurn) {
-        gameboard.classList.add(O_PLAYER);
+        board.classList.add(CIRCLE_CLASS);
+        // changes bottom span text to say new text
         turn.textContent = "It is O's turn";
-    }
-    else {
-        gameboard.classList.add(X_PLAYER);
+        // otherwise use the x class
+    } else {
+        board.classList.add(X_CLASS);
+        // changes bottom span text to say new text
         turn.textContent = "It is X's turn";
     }
 }
 
+// creating functions that checks if currentClass has fulfilled a winning combo
 function checkWin(currentClass) {
-    return WINNING_COMBOS.some(combination => {
+    return WINNING_COMBINATIONS.some(combination => {
         return combination.every(index => {
-            return boxElements[index].classList.contains(currentClass);
+            return cellElements[index].classList.contains(currentClass);
         });
     });
 }
