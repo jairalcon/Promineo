b3215b6c54d18336692753b3d2075b9caabc52c6
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-08-25 17:58:09 -0500

    intial commit

---
+++ 3React/Week13-IntroToReact-Webpack-Components-JSX/Live Class/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import MainArea from './MainArea'
+import Sidebar from './Sidebar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container"> 
+        <div className="row">
+          <Sidebar/>
+          <MainArea/>
+        </div>
+      </div>
+    )
+  }
+}
