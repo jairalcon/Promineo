aee94b1863d1be4c2595302a5c20244c7c4be83d
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-08-29 21:01:24 -0500

    initial commit

---
+++ 3React/Week14-Props-State-Events-LifecycleMethods/week14-project/src/components/main.js
@@ -0,0 +1,11 @@
+//! https://www.simplilearn.com/tutorials/reactjs-tutorial/reactjs-state
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    let 
+    return (
+      <div>main</div>
+    )
+  }
+}
