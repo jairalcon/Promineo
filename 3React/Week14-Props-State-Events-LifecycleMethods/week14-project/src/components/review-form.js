7922450b94315fa71ffa6e443ef39855b980e7e4
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-09-03 19:20:47 -0500

    issue with subimmit from multiple input fields and posting to review-list.js

--- 3React/Week14-Props-State-Events-LifecycleMethods/week14-project/src/components/review-form.js
+++ 3React/Week14-Props-State-Events-LifecycleMethods/week14-project/src/components/review-form.js
@@ -5,25 +5,21 @@ export class ReviewForm extends Component {
         super(props);
 
         this.state = {
-            nicknameValue: '',
-            reviewValue: ''
+            nickname: '',
+            review: ''
         }
     }
 
-    handleNicknameChange = (event) => {
+    handleChange = (event) => {
         // Sets our piece of state to whatever is currently in the textbox
-        this.setState({ reviewValue: event.target.value })
-    }
-
-    handleReviewChange = (event) => {
-        // Sets our piece of state to whatever is currently in the textbox
-        this.setState({ reviewValue: event.target.value })
+        this.setState({ nickname: event.target.nickname });
+        this.setState({ review: event.target.value });
     }
 
     onSubmit = (event) => {
-        event.preventDefault()
-        this.props.addReview(this.state.nicknameValue)
-        this.props.addReview(this.state.reviewValue)
+        event.preventDefault();
+        this.props.addReview(this.state.nickname)
+        this.props.addReview(this.state.review)      
     }
 
   render() {
@@ -32,9 +28,9 @@ export class ReviewForm extends Component {
             <form className='row mt-2' id='review-form'>
                 {/* <h4>Leave a Review:</h4> */}
                 {/* <textarea className='form-control w-25' id='name' rows='1'></textarea> */}
-                <input placeholder='Nickname...' className='w-75' type='text' value={this.state.nicknameValue} onChange={this.handleNicknameChange}/>
-                <textarea placeholder='Leave a Review...' type='text' className='form-control w-75' id='review-form' rows='3' value={this.state.reviewValue} onChange={this.handleReviewChange}></textarea>
-                <button className="col-5 mt-2 btn btn-primary" onClick={this.onSubmit}>Submit</button>
+                <input type='text' placeholder='Nickname...' className='w-75'  value={this.state.nickname} name='nickname' onChange={this.handleChange}/>
+                <textarea type='text' placeholder='Leave a Review...' className='form-control w-75' id='review-form' rows='3' value={this.state.review} name='review' onChange={this.handleChange}></textarea>
+                <button type='submit' className="col-5 mt-2 btn btn-primary" onClick={this.onSubmit}>Submit</button>
             </form>
         </div>
     )
