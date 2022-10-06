6c19d765aecb302d87e3d3fb6fd43095f86de150
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-10-06 17:02:28 -0500

    updating

--- 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/my-test-app/src/Content.js
+++ 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/my-test-app/src/Content.js
@@ -1,34 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Bob', 'Kevin', 'Dave'];
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
+
+    const handleCheck = (id) => {
+        console.log(`key: ${id}`);
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
+        setItems(listItems);
+        // saving to local storage when changes are made
+        localStorage.setItem('shoppingList', JSON.stringify(listItems));
     }
 
-    const handleClick = () => {
-        console.log('You clicked it!');
-    }
-
-    const handleClick2 = (name) => {
-        console.log(`${name} was clicked`);
-    }
-
-    const handleClick3 = (e) => {
-        console.log('e:', e.target.innerText);
+    const handleDelete = (id) => {
+        // console.log('id:', id);
+        // will create a new array that filters item.id === id
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        // saving to local storage when changes are made
+        localStorage.setItem('shoppingList', JSON.stringify(listItems));
     }
 
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
-            </p>
-            <button onClick={handleClick}>Click It</button>
-            <button onClick={() => handleClick2('Dave')}>Click It</button>
-            <button onClick={(e) => handleClick3(e)}>Click It</button>
+            {/* adding message to show that list is empty */}
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className='item' key={item.id}>
+                            <input
+                                type='checkbox'
+                                // passing anon function with functions passing in item.id
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label 
+                                onDoubleClick={() => handleCheck(item.id)}
+                                style={(item.checked)? { textDecoration: 'line-through' } : null}
+                            >{item.item}</label>
+                            <FaTrashAlt
+                                onClick={() => handleDelete(item.id)} 
+                                role='button' 
+                                tabIndex='0' 
+                            />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty</p>
+            )}
         </main>
-    )
+    );
 }
 
 export default Content;
