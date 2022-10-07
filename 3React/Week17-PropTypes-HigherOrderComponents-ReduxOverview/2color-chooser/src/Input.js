43f5403ecb9f2fc1d41e6d3f9736d3a6d162e0b3
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-10-07 10:57:51 -0500

    updating

--- 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/2color-chooser/src/Input.js
+++ 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/2color-chooser/src/Input.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import colorNames from 'colornames';
 
-const Input = ({ colorValue, setColorValue }) => {
+const Input = ({ colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText }) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
         <label>Add Color Name:</label>
@@ -10,8 +11,17 @@ const Input = ({ colorValue, setColorValue }) => {
             placeholder='Add color name'
             required
             value={colorValue}
-            onChange={(e) => setColorValue(e.target.value)}
+            onChange={(e) => {
+                setColorValue(e.target.value)
+                setHexValue(colorNames(e.target.value))
+            }}
         />
+        <button
+            type='button'
+            onClick={() => setIsDarkText(!isDarkText)}
+        >
+            Toggle Text Color
+        </button>
     </form>
   )
 }
