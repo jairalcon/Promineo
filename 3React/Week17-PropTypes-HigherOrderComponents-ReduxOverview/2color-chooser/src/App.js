5c6b1de9c6e9cc766aa35b859731db9ea5284705
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-10-07 10:46:52 -0500

    initial commit

---
+++ 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/2color-chooser/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Input from './Input';
+import Square from './Square';
+import { useState } from 'react';
+
+function App() {
+  const [ colorValue, setColorValue ] = useState('')
+  return (
+    <div className='App'>
+      <Square colorValue={colorValue}/>
+      <Input 
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+      />
+    </div>
+  );
+}
+
+export default App;
