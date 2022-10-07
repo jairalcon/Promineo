360841af2daa55f7cd12bf36fcebeac5cb1635b6
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-10-07 15:31:41 -0500

    updating

--- 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/my-test-app/src/App.js
+++ 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/my-test-app/src/App.js
@@ -4,10 +4,10 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
-
 
   const [items, setItems] = useState([]);
   // moved from Content.js and drilled down
@@ -43,25 +43,53 @@ function App() {
   }, [])
   // console.log('after useEffect')
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if(result) setFetchError(result);
   }
 
   // moved from Content.js and drilled down 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     console.log(`key: ${id}`);
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked})
+    };
+    // accessing specific post with patch
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     // console.log('id:', id);
     // will create a new array that filters item.id === id
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
