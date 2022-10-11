fedd10038e75312d042fec22a46158499a761988
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-10-11 16:37:57 -0500

    updating

--- 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/3react-js-blog/src/App.js
+++ 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/3react-js-blog/src/App.js
@@ -1,6 +1,4 @@
-import Header from './Header';
-import Nav from './Nav';
-import Footer from './Footer';
+import Layout from './Layout';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
@@ -11,32 +9,7 @@ import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
-  ])
+  const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
@@ -70,24 +43,29 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Header title="React JS Blog" />
-      <Nav search={search} setSearch={setSearch} />
-      <Routes>
-        <Route path='/' element={<Home posts={searchResults} />} />
-        <Route path='/post' element={<NewPost
-          handleSubmit={handleSubmit}
-          postTitle={postTitle}
-          setPostTitle={setPostTitle}
-          postBody={postBody}
-          setPostBody={setPostBody}
-        />} />
-        <Route path='/post/:id' element={<PostPage posts={posts} handleDelete={handleDelete} />} />
-        <Route path='/about' element={<About />} />
+    <Routes>
+      <Route path='/' element={<Layout
+        search={search}
+        setSearch={setSearch}
+      />}>
+        <Route index element={<Home posts={searchResults} />} />
+        <Route path='post'>
+          <Route index element={<NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />} />
+          <Route path=':id' element={<PostPage
+            posts={posts}
+            handleDelete={handleDelete}
+          />} />
+        </Route>
+        <Route path='about' element={<About />} />
         <Route path='*' element={<Missing />} />
-      </Routes>
-      <Footer />
-    </div>
+      </Route>
+    </Routes>
   );
 }
 
