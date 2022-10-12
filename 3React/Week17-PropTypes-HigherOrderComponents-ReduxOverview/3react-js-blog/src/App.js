85381f27af079d05a897b63f24705a8544186779
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-10-12 09:23:53 -0500

    updating

--- 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/3react-js-blog/src/App.js
+++ 3React/Week17-PropTypes-HigherOrderComponents-ReduxOverview/3react-js-blog/src/App.js
@@ -10,6 +10,7 @@ import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
 import useWindowSize from './hooks/useWindowSize';
+import useAxiosFetch from './hooks/useAxiosFetch';
 // ! npx json-server -p 3500 -w data/db.json
 // https://www.npmjs.com/package/react-use
 
@@ -23,26 +24,12 @@ function App() {
   const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
   const { width } = useWindowSize();
+
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
   
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch(err) {
-        if (err.response) {
-        // Not in the 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
-
-    fetchPosts();
-  }, [])
+    setPosts(data);
+  }, [data])
 
   useEffect(() => {
     const filteredResults = posts.filter(post =>
@@ -104,7 +91,11 @@ function App() {
         setSearch={setSearch}
         width={width}
       />}>
-        <Route index element={<Home posts={searchResults} />} />
+        <Route index element={<Home 
+          posts={searchResults}
+          fetchError={fetchError}
+          isLoading={isLoading}
+        />} />
         <Route path='post'>
           <Route index element={<NewPost
             handleSubmit={handleSubmit}
@@ -132,5 +123,26 @@ function App() {
     </Routes>
   );
 }
+
+ // !Not using because of customHook
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await api.get('/posts');
+  //       setPosts(response.data);
+  //     } catch(err) {
+  //       if (err.response) {
+  //       // Not in the 200 response range
+  //         console.log(err.response.data);
+  //         console.log(err.response.status);
+  //         console.log(err.response.headers);
+  //       } else {
+  //         console.log(`Error: ${err.message}`);
+  //       }
+  //     }
+  //   }
+
+  //   fetchPosts();
+  // }, [])
 
 export default App;
